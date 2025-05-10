2184b57656d5859f75cd2ad0b2073ef8e0ab47c5
Izumi Zama <dev@izumiz.me>
2025-05-10 14:45:54 +0900

    chore: add pnpm workspace configuration and update scripts

--- scripts/blog-contents-cache.cjs
+++ scripts/blog-contents-cache.cjs
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-var-requires */
 const { exec } = require('child_process');
 const { Client } = require('@notionhq/client');
 const cliProgress = require('cli-progress');
--- scripts/retrieve-block-children.cjs
+++ scripts/retrieve-block-children.cjs
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const fs = require('fs');
 const { setTimeout } = require('timers/promises');
 const { Client } = require('@notionhq/client');
